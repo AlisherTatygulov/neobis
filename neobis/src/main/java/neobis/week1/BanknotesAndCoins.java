47d36e69c65fecd4adf1b96e815faf07ccc25444
AlisherTatygulov <Alisher199026@gmail.com>
2024-05-02 20:43:11 +0500

    Solution of 'Banknotes' and 'BanknotesAndCoins'

--- neobis/src/main/java/neobis/week1/BanknotesAndCoins.java
+++ neobis/src/main/java/neobis/week1/BanknotesAndCoins.java
@@ -1,23 +1,39 @@
 package neobis.week1;
 
 import java.io.IOException;
+import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class BanknotesAndCoins {
     public static void main(String[] args) throws IOException {
 
-        int[] notes = {100, 50, 20, 10, 5, 2};
-        double[] coins = {1, 0.50, 0.25, 0.10, 0.05, 0.01};
+        int[] banknotes = {100, 50, 20, 10, 5, 2};
+        double[] bankcoins = {1.00, 0.50, 0.25, 0.10, 0.05, 0.01};
 
         Scanner sc = new Scanner(System.in);
-        int money = sc.nextInt();
-        System.out.println(money);
+        double money = sc.nextDouble();
+        int notes = (int) money;
+        double coins = money - notes;
 
-        System.out.println("NOTAS");
-        for (int note: notes) {
-            int count = money / note;
-            System.out.println(count + " nota(s) de R$ " + note + ".00");
-            money = money % note;
+        System.out.println("NOTAS:");
+        for (int banknote: banknotes) {
+            int count = notes / banknote;
+            System.out.println(count + " nota(s) de R$ " + banknote + ".00");
+            notes = notes % banknote;
+        }
+
+         DecimalFormat df = new DecimalFormat("0.00");
+
+
+        coins = notes + coins;
+
+        System.out.println("MOEDAS:");
+        for (double bankcoin: bankcoins) {
+            int count = (int) (coins / bankcoin);
+
+            String formattedNumber = df.format(bankcoin);
+            System.out.println(count + " moeda(s) de R$ " + formattedNumber);
+            coins = coins % bankcoin;
         }
 
 
