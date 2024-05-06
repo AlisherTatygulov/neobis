f77751537394ef25acf0428ed6bc09c4ffd1c02e
iminovvan <105599313+iminovvan@users.noreply.github.com>
2024-05-06 20:58:01 +0500

    Update SequenceIJ4.java

--- neobis/src/main/java/neobis/week1/SequenceIJ4.java
+++ neobis/src/main/java/neobis/week1/SequenceIJ4.java
@@ -1,10 +1,12 @@
 package neobis.week1;
+import java.text.DecimalFormat;
 
 public class SequenceIJ4 {
     public static void main(String[] args) {
         for (double i = 0; i <= 2; i += 0.2) {
             for (int j = 1; j <= 3; j++) {
-                System.out.printf("I=%.1f J=%.1f\n", i, i + j);
+                DecimalFormat df = new DecimalFormat("#.#");
+                System.out.println("I=" + df.format(i) + " J=" + df.format(i+j));
             }
         }
     }
